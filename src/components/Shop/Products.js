c666d068813c78b92c2b2dcf139576b51648794d
GioZhorzholiani3 <giorgi.zhorzholiani.1@btu.edu.ge>
2022-09-30 15:12:35 +0400

    first commit

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,38 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "first product",
+    description: "first product i ever bought",
+  },
+  {
+    id: "p2",
+    price: 7,
+    title: "second product",
+    description: "second product i ever bought",
+  },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id} //?radgav vmapavt yvela produqts unda qondes taviki kay
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
